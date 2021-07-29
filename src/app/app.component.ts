2e47abeee84228cabfa000aeda2f4f4ce94b1cf9
Samuel Fernandes <samueldiablo73@gmail.com>
2021-07-29 13:37:31 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,20 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 // import axios from 'axios';
 
-
-
 export class Post {
   constructor(
     public title: string,
     public content: string,
     public author: string,
     public id: number
+  ) { }
+}
+
+export class User {
+  constructor(
+    public username: string,
+    public email: string,
+    public password: string,
   ) { }
 }
 
@@ -26,6 +32,8 @@ export class AppComponent implements OnInit {
 
   posts: Post[] = [];
   formdata!: FormGroup;
+  users: User[] = [];
+
 
   constructor(
     private httpClient: HttpClient
@@ -37,6 +45,31 @@ export class AppComponent implements OnInit {
       emailid: new FormControl("angular@gmail.com"),
       passwd: new FormControl("abcd1234")
     });
+  }
+
+  registerForm(form: any) {
+    var json = JSON.stringify({
+      username: form.value.username,
+      email: form.value.email,
+      password: form.value.password
+    });
+    var options = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic YWRtaW46MTU5NzUzZ290NDI='
+      },
+    }
+    this.httpClient.post<any>('http://localhost:8000/register/', json, options).subscribe(
+      response => {
+        console.log(response);
+        this.users = response;
+       
+        if (window.confirm('Usuário cadastrado com sucesso!')) {
+          form.reset();
+          window.location.reload();
+        }
+      }
+    );
   }
 
 
